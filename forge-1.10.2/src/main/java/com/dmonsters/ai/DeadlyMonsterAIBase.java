cce7b86ba146e1894e4385e10c655bb7e975117a
asanetargoss <asanetargoss@users.noreply.github.com>
2020-10-22 22:04:10 -0700

    Only let monster meelee attack if there is no wall in the way

--- forge-1.10.2/src/main/java/com/dmonsters/ai/DeadlyMonsterAIBase.java
+++ forge-1.10.2/src/main/java/com/dmonsters/ai/DeadlyMonsterAIBase.java
@@ -1,6 +1,32 @@
 package com.dmonsters.ai;
 
+import java.util.Random;
+
+import com.dmonsters.main.MainMod;
+
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.util.math.Vec3d;
 
 public abstract class DeadlyMonsterAIBase extends EntityAIBase {
+    public static boolean canAttack(Random random, EntityLivingBase attacker, EntityLivingBase target) {
+        if (attacker.worldObj != target.worldObj || attacker.worldObj == null) {
+            MainMod.logger.error("Deadly monster " + attacker.getClass().getName() +
+                    " attempted to attack target " + target.getClass().getName() +
+                    " in another world.");
+            return false;
+        }
+        int i = 0;
+        float heightFraction = 0.5F;
+        do {
+            Vec3d start = new Vec3d(attacker.posX, attacker.lastTickPosY + (heightFraction * attacker.height), attacker.posZ);
+            Vec3d end = new Vec3d(target.posX, target.lastTickPosY + (heightFraction * target.height), target.posZ);
+            if (attacker.worldObj.rayTraceBlocks(start, end, false, true, false) == null) {
+                return true;
+            }
+            heightFraction = random.nextFloat();
+            i++;
+        } while (i < 3);
+        return false;
+    }
 }
--- forge-1.10.2/src/main/java/com/dmonsters/ai/DeadlyMonsterAIMeelee.java
+++ forge-1.10.2/src/main/java/com/dmonsters/ai/DeadlyMonsterAIMeelee.java
@@ -1,11 +1,25 @@
 package com.dmonsters.ai;
 
+import java.util.Random;
+
 import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
 
 public class DeadlyMonsterAIMeelee extends EntityAIAttackMelee
 {
+    protected Random random = new Random();
+
     public DeadlyMonsterAIMeelee(EntityCreature creature, double speed, boolean useLongMemory) {
         super(creature, speed, useLongMemory);
+    }
+    
+    @Override
+    protected void checkAndPerformAttack(EntityLivingBase target, double distanceSquared) {
+        double maxDistanceSquared = this.getAttackReachSqr(target);
+
+        if (distanceSquared <= maxDistanceSquared && DeadlyMonsterAIBase.canAttack(random, attacker, target)) {
+            super.checkAndPerformAttack(target, distanceSquared);
+        }
     }
 }
